09ffd3a63701607eddb4227a772ae4b769b5344f
Niko Ugarov <xxkkoo@gmail.com>
2021-08-31 22:04:18 +0200

    Main looks a bit more functional with tap()

--- src/main/scala/wunderreise/Main.scala
+++ src/main/scala/wunderreise/Main.scala
@@ -1,24 +1,28 @@
 package wunderreise
 
   object Main {
+
     def dirSymbol(dir:Direction): String = dir match {
       case Right => ">"
       case Left => "<"
       case _ => "-"
     }
 
+    def tap(scheduler: Scheduler):Scheduler = {
+      println(scheduler.trains.map(t => "%3d %1s ".format(t.position, dirSymbol(t.direction))).mkString("|"))
+      scheduler
+    }
+
     def main(args: Array[String]): Unit = {
       if( args.length>0 && args.forall(_.forall(_.isDigit)))  {
         val trains = args.map(_.toInt).map(Train.apply).toIndexedSeq
         val pair = """(\d+)\s+(\d+)""".r
-        io.Source.stdin.getLines().foldLeft(Scheduler(trains:_*)) { (sch, input) =>
-          val next =  if (input.isEmpty) sch.next else {
+        io.Source.stdin.getLines().foldLeft(tap(Scheduler(trains:_*))) { (sch, input) =>
+          tap( if (input.isEmpty) sch.next else {
             val pickups = input.split("[;,]").map(_.trim).toIndexedSeq
                                .map { case pair(from, to) => from.toInt -> to.toInt }
             sch.pickup(pickups: _*).next
-          }
-          println(next.trains.map(t => "%3d %1s ".format(t.position,dirSymbol(t.direction))).mkString("|"))
-          next
+          })
         }
       } else {
         println("Please provide starting positions for the trains as parameters.\n" +
@@ -29,4 +33,4 @@ package wunderreise
 
     }
 
-}
+  }
