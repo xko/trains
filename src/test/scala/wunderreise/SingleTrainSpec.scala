6734a14ca85e58af9bacbda4d74670a9e74ac88c
Niko Ugarov <xxkkoo@gmail.com>
2021-08-31 19:15:08 +0200

    a little reorg in SingleTrainSpec
    there will be not only optimality tests

--- src/test/scala/wunderreise/SingleTrainSpec.scala
+++ src/test/scala/wunderreise/SingleTrainSpec.scala
@@ -40,45 +40,44 @@ class SingleTrainSpec extends AnyFunSpec with Matchers with ScalaCheckPropertyCh
     }
   }
 
-  describe("with some particular requests, serves optimally") {
-    it("6->7, 5->4; starting at 4") {
-      route(Train(4).assign(6->7).assign(5->4)) shouldEqual
-        List(4, 5, 4, 5, 6, 7)
+  describe("with some particular requests ") {
+    describe("serves optimally") {
+      it("6->7, 5->4; starting at 4") {
+        route(Train(4).assign(6 -> 7).assign(5 -> 4)) shouldEqual
+          List(4, 5, 4, 5, 6, 7)
+      }
+
+      it("11->12, 7->9; starting at 10") {
+        route(Train(10).assign(11 -> 12).assign(7 -> 9)) shouldEqual
+          List(10, 11, 12, 11, 10, 9, 8, 7, 8, 9)
+      }
+
+      it("13->20,12->8; starting at 10") {
+        route(Train(10).assign(13 -> 20).assign(12 -> 8)) shouldEqual
+          List(10, 11, 12, 11, 10, 9, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
+      }
+
+      it("6->1,12->0; starting at 4") {
+        route(Train(4).assign(6 -> 1).assign(12 -> 0)) shouldEqual
+          List(4, 5, 6, 7, 8, 9, 10, 11, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+
+      }
+
+      it("6->0, 12->2; starting at 4") {
+        route(Train(4).assign(6 -> 0).assign(12 -> 2)) shouldEqual
+          List(4, 5, 6, 7, 8, 9, 10, 11, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
+      }
+
+      it("20->21, 26->20, 19->20; starting at 21 ") {
+        route(Train(21).assign(20 -> 21).assign(26 -> 20).assign(19 -> 20)) shouldEqual
+          List(21, 22, 23, 24, 25, 26, 25, 24, 23, 22, 21, 20, 19, 20, 21)
+      }
+
+      it("11->4, 5->1, 2->10; starting at 6") {
+        route(Train(6).assign(11 -> 4).assign(5 -> 1).assign(2 -> 10)) shouldEqual
+          List(6, 5, 4, 3, 2, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 10, 9, 8, 7, 6, 5, 4)
+      }
     }
-
-    it("11->12, 7->9; starting at 10") {
-      route(Train(10).assign(11->12).assign(7->9)) shouldEqual
-        List(10, 11, 12, 11, 10, 9, 8, 7, 8, 9)
-    }
-
-    it("13->20,12->8; starting at 10") {
-      route(Train(10).assign(13->20).assign(12->8)) shouldEqual
-        List(10, 11, 12, 11, 10, 9, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20)
-    }
-
-    it("6->1,12->0; starting at 4"){
-      route(Train(4).assign(6->1).assign(12->0)) shouldEqual
-        List(4, 5, 6, 7, 8, 9, 10, 11, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-
-    }
-
-    it("6->0, 12->2; starting at 4"){
-      route(Train(4).assign(6->0).assign(12->2)) shouldEqual
-        List(4, 5, 6, 7, 8, 9, 10, 11, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0)
-    }
-
-    it("20->21, 26->20, 19->20; starting at 21 ") {
-      route(Train(21).assign(20->21).assign(26->20).assign(19->20)) shouldEqual
-        List(21,22,23,24,25,26,25,24,23,22,21,20,19,20,21)
-    }
-
-    it("11->4, 5->1, 2->10; starting at 6") {
-      route(Train(6).assign(11->4).assign(5->1).assign(2->10)) shouldEqual
-        List(6,5,4,3,2,1,2,3,4,5,6,7,8,9,10,11,10,9,8,7,6,5,4)
-
-    }
-
-
   }
 
   describe("with many random requests") {
